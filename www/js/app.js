1dfed6f4007659f8052d6d676905da7b3ede2d11
Grégory <greg.pennetier@gmail.com>
2017-01-18 15:40:44 +0100

    blabal

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
+	alert("TOTO");
 
 	document.addEventListener("deviceready", onDeviceReady, false);
 	function onDeviceReady(){
-	alert("TOTO");
 	var myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
 
-	myDB.transaction(function(transaction) {
-		transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
-		function(tx, result) {
-			alert("Table created successfully");
-		},
-		function(error) {
-			alert("Error occurred while creating the table.");
+		myDB.transaction(function(transaction) {
+			transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
+			function(tx, result) {
+				alert("Table created successfully");
+			},
+			function(error) {
+				alert("Error occurred while creating the table.");
+			});
 		});
-	});
 	};
 
 
