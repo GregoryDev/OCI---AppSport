2eed4f79f523393f8ef93d64e393f846a38771bd
guillaume <guillaume.le.roux@hotmail.fr>
2017-01-19 09:04:40 +0100

    Js pour le submlit & debut description event

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
 
-	alert("TOTO");
+    //On submit create event form
+    //TODO: ranger ca dans la bdd
+    $("#create_event").submit(function( event ) {
+          //alert( "Handler for .submit() called." );
+    });
+    $("#create_event").hide();
+        
 
-	document.addEventListener('deviceready', function() {
-	  myDB = window.sqlitePlugin.openDatabase({name: 'demo.db', location: 'default'});
-	});
+    document.addEventListener('deviceready', function() {
+        myDB = window.sqlitePlugin.openDatabase({name: 'demo.db', location: 'default'});
+    });
 
-	alert("tata");
-	myDB.transaction(function(transaction) {
-		transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
-		function(tx, result) {
-			alert("Table created successfully");
-		},
-		function(error) {
-			alert("Error occurred while creating the table.");
-		});
-	});
+    alert("tata");
+    myDB.transaction(function(transaction) {
+        transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
+                function(tx, result) {
+                    alert("Table created successfully");
+                },
+                function(error) {
+                    alert("Error occurred while creating the table.");
+                });
+    });
 
-	alert('tutu');
+    alert('tutu');
 
 });
