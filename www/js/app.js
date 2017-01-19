12b775a2d15a477f0bfd329e67a49eb125aafd33
guillaume <guillaume.le.roux@hotmail.fr>
2017-01-19 11:41:33 +0100

    commit

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	function initialize(){		
     	$("#div_create_event").hide();
-    	$("#desc_event").hide();
+    	//$("#desc_event").hide();
     	$("#listing_event").hide();
     	$("#div_create_user").hide();
     	$("#menu").hide();
