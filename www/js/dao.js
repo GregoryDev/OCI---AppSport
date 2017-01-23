241ad1c45afe0f01fe9564eb8a267d10952296c4
Yoan-dev <yoanbocquelet@gmail.com>
2017-01-23 13:54:53 +0100

    update db

--- www/js/dao.js
+++ www/js/dao.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
 
         // Cordova is ready
         function onDeviceReady() {
-            var db = window.openDatabase("Database", "1.0", "AppSport", 200000);
+            var db = window.openDatabase("Database", "1.0", "AppSport", 2000000);
             db.transaction(populateDB, errorCB, successCB);
         }
 
         // Populate the database
         function populateDB(tx) {
-	    tx.executeSql('DROP TABLE events');
-            tx.executeSql('CREATE TABLE events (id INTEGER PRIMARY KEY AUTOINCREMENT, nom_event, date_event, nbPlace_event, prix_event, description_event)');
+	    //tx.executeSql('DROP TABLE events');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, nom_event text, date_event date, nbPlace_event integer, prix_event integer, description_event text)');
         }
-// IF NOT EXISTS
+// 
 
         // Query the success callback
         function querySuccess(tx, results) {
@@ -28,28 +28,28 @@ $(document).ready(function() {
 
         // Transaction error callback
         function errorCB(err) {
-            alert("Error processing SQL: "+err.code);
+            alert("Error processing SQL: "+err.code + ": " + err.message);
         }
 
         // Transaction success callback
         function successCB() {
-            var db = window.openDatabase("Database", "1.0", "AppSport", 200000);
+            var db = window.openDatabase("Database", "1.0", "AppSport", 2000000);
             db.transaction(queryDB, errorCB);
         }
 
 	// Insert event
         $("#submit_event").click(function goInsertEvent() {
-            var db = window.openDatabase("Database", "1.0", "AppSport", 200000);
+            var db = window.openDatabase("Database", "1.0", "AppSport", 2000000);
             db.transaction(insertEvent, errorCB, querySuccess);
         })
         function insertEvent(tx) {
-            tx.executeSql('INSERT INTO events (name) VALUES ("' +
-		$("#nom_event").value +'","'+
-		$("#date_event").value +'","'+
-		$("#nbPlace_event").value +'","'+
-		$("#prix_event").value +'","'+
-		$("#description_event").value+'")');
+            tx.executeSql('INSERT INTO events (nom_event, date_event, nbPlace_event, prix_event, description_event) VALUES ("' +
+		document.getElementById("nom_event").value +'","'+
+		document.getElementById("date_event").value +'","'+
+		document.getElementById("nbPlace_event").value +'","'+
+		document.getElementById("prix_event").value +'","'+
+		document.getElementById("description_event").value+'")');
         }
-	//
+	//document.getElementById
 
 });
