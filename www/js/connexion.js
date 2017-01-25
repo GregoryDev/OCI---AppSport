3a6fdd5898a38623478d09566c58f954c0c9cffc
Yoan-dev <yoanbocquelet@gmail.com>
2017-01-25 15:15:26 +0100

    createur event + debut connexion

---
+++ www/js/connexion.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // Query the success callback
+    function querySuccess(tx, results) {
+        console.log(results);
+    }
+
+    // Transaction error callback
+    function errorCB(err) {
+        alert("Error processing SQL: "+err.code + ": " + err.message);
+    }
+
+    // Connexion
+    $("#connexion").click(function goInsertUser() {
+        var db = window.openDatabase("Database", "1.0", "AppSport", 2000000);
+        db.transaction(insertUser, errorCB, querySuccess);
+    })
+
+    // Deconnexion
+    $("#deconnexion").click(function () {
+	//
+    })
+});
+
--- www/js/event.js
+++ www/js/event.js
@@ -20,12 +20,13 @@ $(document).ready(function() {
         db.transaction(insertEvent, errorCB, querySuccess);
     })
     function insertEvent(tx) {
-        tx.executeSql('INSERT INTO events (nom_event, type_event, date_event, nbPlace_event, prix_event, description_event) VALUES ("' +
-                    document.getElementById("nom_event").value +'","'+
-                    document.getElementById("type_event").value +'","'+
-                    document.getElementById("date_event").value +'","'+
-                    document.getElementById("nbPlace_event").value +'","'+
-                    document.getElementById("prix_event").value +'","'+
-                    document.getElementById("description_event").value+'")');
+        tx.executeSql('INSERT INTO events (nom_event, type_event, date_event, nbPlace_event, prix_event, description_event, creator_event) VALUES ("' +
+                    $("#nom_event").value +'","'+
+                    $("#type_event").value +'","'+
+                    $("#date_event").value +'","'+
+                    $("#nbPlace_event").value +'","'+
+                    $("#prix_event").value +'","'+
+                    $("#description_event").value +'","'+
+                    $("#logged").attr("class")+'")');
     }
 });
